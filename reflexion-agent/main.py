33ce44acfb9f9b351deca5e33a9c7642629a3e64
Dyggas <Dyggas@gmail.com>
2025-09-05 17:48:55 +0300

    Added nodes and edges [DEV]

---
+++ reflexion-agent/main.py
@@ -0,0 +1,57 @@
+from dotenv import load_dotenv
+
+from typing import TypedDict, Annotated
+
+from langchain_core.messages import BaseMessage, ToolMessage
+from langgraph.graph import END, StateGraph
+from langgraph.graph.message import add_messages
+
+from chains import revisor, first_responder
+from tool_executor import execute_tools
+
+load_dotenv()
+
+MAX_ITERATIONS = 2
+
+DRAFT = "draft"
+EXECUTE_TOOLS = "execute tools"
+REVISE = "revise"
+
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+def first_responder_node(state: State):
+    return first_responder.invoke({"messages" : state["messages"]})
+
+def execute_tools_node(state: State):
+    return execute_tools.invoke({"messages" : state["messages"]})
+
+def revise_node(state: State):
+    return revisor.invoke({"messages" : state["messages"]})
+
+builder = StateGraph(State)
+builder.add_node(DRAFT, first_responder_node)
+builder.add_node(EXECUTE_TOOLS, execute_tools_node)
+builder.add_node(REVISE, revise_node)
+
+builder.set_entry_point(DRAFT)
+
+builder.add_edge(DRAFT, EXECUTE_TOOLS)
+builder.add_edge(EXECUTE_TOOLS, REVISE)
+
+def event_loop(state: State):
+    count_tool_visits = sum(isinstance(item, ToolMessage) for item in state["messages"])
+    num_iterations = count_tool_visits
+    if num_iterations > MAX_ITERATIONS:
+        return END
+    return EXECUTE_TOOLS
+
+builder.add_conditional_edges(REVISE, event_loop, {END:END, EXECUTE_TOOLS:EXECUTE_TOOLS})
+
+graph = builder.compile()
+
+print(graph.get_graph().draw_ascii())
+
+if __name__ == "__main__":
+    print("Hello World")
