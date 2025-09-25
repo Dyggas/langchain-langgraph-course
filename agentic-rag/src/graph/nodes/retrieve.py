66c08b4b93dbc3d98ff3e828bd6ea9a87b6369ba
Dyggas <Dyggas@gmail.com>
2025-09-25 13:32:31 +0300

    fix: update import statement for retriever to use get_retriever function

--- agentic-rag/src/graph/nodes/retrieve.py
+++ agentic-rag/src/graph/nodes/retrieve.py
@@ -1,12 +1,13 @@
 from typing import Any, Dict
 
 from graph.state import GraphState
-from ingestion import retriever
+from ingestion import get_retriever
 
 
 def retrieve(state: GraphState) -> Dict[str, Any]:
     print("---RETRIEVE---")
     question = state["question"]
 
+    retriever = get_retriever()
     documents = retriever.invoke(question)
     return {"documents": documents}
