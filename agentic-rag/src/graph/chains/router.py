ae310dab7129405869d6416e11dd80cae91fc95d
Dyggas <Dyggas@gmail.com>
2025-10-01 11:59:03 +0300

    feat: implement routing functionality for user queries to relevant datasources

---
+++ agentic-rag/src/graph/chains/router.py
@@ -0,0 +1,37 @@
+from typing import Literal
+
+from langchain_core.prompts import ChatPromptTemplate
+from pydantic import BaseModel, Field
+from langchain_openai import AzureChatOpenAI
+from langchain_core.runnables import RunnableSequence
+import os
+
+llm = AzureChatOpenAI(
+    azure_deployment=os.getenv("AZURE_OPENAI_CHAT_DEPLOYMENT"),
+    api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
+    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+    temperature=0,
+)
+
+class RouteQuery(BaseModel):
+    """Route a user query to the most relevant datasource."""
+
+    datasource: Literal["vectorstore", "websearch"] = Field(
+        ...,
+        description="Given a user question, decide whether to route it to the vectorstore or websearch to answer the question. ",
+    )
+
+structured_llm_router = llm.with_structured_output(RouteQuery)
+
+
+system = """You are an expert at routing a user question to a vectorstore or web search.
+The vectorstore contains documents related to agents, prompt engineering, and adversarial attacks.
+Use the vectorstore for questions on these topics. For all else, use web-search."""
+route_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system),
+        ("human", "{question}"),
+    ]
+)
+
+question_router: RunnableSequence = route_prompt | structured_llm_router # type: ignore
